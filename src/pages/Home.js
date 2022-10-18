40129d3c8c721b325b561689aaa588aefdebbef0
siripol <siripol.t@siamkubota.co.th>
2022-10-18 19:38:43 +0700

    before rn-nodeify using

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,15 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import Footer from '../components/Footer'
+import Header from '../components/Header'
+import Content from '../components/Content'
 
-export default function Home() {
+
+export default function Home({id}) {
   return (
     <View>
-      <Text>Home</Text>
-      
+      <Header title={"NFC data"}/>
+      <Content title={"id"} content={id}/>
       <Footer/>
     </View>
   )
